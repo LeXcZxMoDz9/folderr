750f361063c062aca5ca0863f49424f6cc240c65
LeXcZxMoDz9 <bintangkrn5@gmail.com>
2024-07-23 05:04:28 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,23 +4,14 @@
 ALLOWED_IPS=(
     "68.183.157.251"
     "159.65.134.147"
-    "157.245.193.33"
-    "-"
-    "-"
-    "-"
-    "-"
-    "-"
-    "-"
-    "-"
-    "-"
-    "-"
 )
 
 # Tentukan lisensi yang valid
-VALID_LICENSE="LEX"
+VALID_LICENSE="lex"
 
-# Path file lisensi
+# Path file lisensi dan file kesalahan
 LICENSE_FILE="/var/www/pterodactyl/license.txt"
+ERROR_FILE="/var/www/pterodactyl/error_count.txt"
 
 # Ambil alamat IP saat ini
 CURRENT_IP=$(hostname -I | awk '{print $1}')
@@ -58,15 +49,37 @@ if ! is_ip_allowed "$CURRENT_IP"; then
     exit
 fi
 
-# Verifikasi lisensi
-if ! is_license_valid; then
-    echo "Lisensi tidak valid atau belum dimasukkan!"
-    if [[ ! -f "$LICENSE_FILE" ]]; then
-        echo "$VALID_LICENSE" > "$LICENSE_FILE"
-        echo "Lisensi telah ditambahkan untuk 24 jam."
-    fi
+# Inisialisasi file kesalahan jika tidak ada
+if [[ ! -f "$ERROR_FILE" ]]; then
+    echo "0" > "$ERROR_FILE"
 fi
 
+# Verifikasi lisensi
+if ! is_license_valid; then
+    ERROR_COUNT=$(cat "$ERROR_FILE")
+    ERROR_COUNT=$((ERROR_COUNT + 1))
+    echo "$ERROR_COUNT" > "$ERROR_FILE"
+    if [[ $ERROR_COUNT -ge 3 ]]; then
+        echo -e "\033[31mLisensi tidak valid atau belum dimasukkan! Anda telah gagal 3 kali. Anda akan logout.\033[0m"
+        for i in 3 2 1; do
+            echo "$i"
+            sleep 1
+        done
+        logout
+        exit
+    else
+        echo -e "\033[31mLisensi tidak valid atau belum dimasukkan! Anda telah salah $ERROR_COUNT kali. Sisa $(($ERROR_COUNT)) kali lagi.\033[0m"
+    fi
+else
+    # Reset error count jika lisensi valid
+    echo "0" > "$ERROR_FILE"
+fi
+
+# Tambahkan lisensi jika belum ada dan berikan informasi
+if [[ ! -f "$LICENSE_FILE" ]]; then
+    echo "$VALID_LICENSE" > "$LICENSE_FILE"
+    echo "Lisensi telah ditambahkan untuk 24 jam."
+fi
 # Tampilkan teks setelah loading selesai
 display_text
 # Definisi warna untuk tampilan teks
@@ -110,10 +123,15 @@ load_config
 # Menampilkan banner
 clear
 echo -e "\033[31m"
-echo " ╾━╤デ╦︻"
-echo " IM LEXCZ FROM INDONESIA HACKER"
-echo " ╾━╤デ╦︻"
-echo " IM LEXCZ FROM INDONESIA HACKER"
+echo " |  _ \    / \  |_ _| \ | |  \/  |/ ___|"
+echo " | |_) |  / _ \  | ||  \| | |\/| | |"
+echo " |  _ <  / ___ \ | || |\  | |  | | |___"
+echo " |_| \_\/_/   \_\___|_| \_|_|  |_|\____|"
+echo "  ____  ____  _____ __  __ ___ _   _ __  __"
+echo " |  _ \|  _ \| ____|  \/  |_ _| | | |  \/  |"
+echo " | |_) | |_) |  _| | |\/| || || | | |\/| |"
+echo " |  __/|  _ <| |___| |  | || || |_| | |  | |"
+echo " |_|   |_| \_\_____|_|  |_|___|\___/|_|  |_|"
 echo -e "\033[0m"
 echo -e "\033[32mWHATSAPP : 087743212449\033[0m"
 echo -e "\033[31mYOUTUBE : LEXCZXMODZ\033[0m"
@@ -187,11 +205,12 @@ echo "5. INSTALL GOOGLE ANALITIC PTERODACTYL"
 echo "   ᴘʀᴇᴠɪᴇᴡ : https://builtbybit.com/resources/google-analytics-addon-for-pterodactyl.38696/"
 echo "6. ADMIN PANEL THEME PTERODACTYL"
 echo "   ᴘʀᴇᴠɪᴇᴡ : https://builtbybit.com/resources/slate.36101/"
-echo "7. HAPUS BACKROUND PTERODACTYL (RESET BACKROUND JADI YANG AWAL)"
-echo "8. HAPUS THEME/ADDON"
-echo "9. MATIKAN SEMUA ANIMASI INSTALLER (TIDAK BERLAKU DI WEB ANDA HANYA MEMATIKAN TEXT ANIMATION INSTALLER)"
-echo "10. KELUAR DARI INSTALLER"
-read -p "PILIH OPSI (1-10): " OPTION
+echo "7. ENIGMA PREMIUM PTERODACTYL REMAKE BY RAINSTOREID"
+echo "8. HAPUS BACKROUND PTERODACTYL (RESET BACKROUND JADI YANG AWAL)"
+echo "9. HAPUS THEME/ADDON"
+echo "10. MATIKAN SEMUA ANIMASI INSTALLER (TIDAK BERLAKU DI WEB ANDA HANYA MEMATIKAN TEXT ANIMATION INSTALLER)"
+echo "11. KELUAR DARI INSTALLER"
+read -p "PILIH OPSI (1-11): " OPTION
 case "$OPTION" in
     1)
         # Masukkan token GitHub langsung di sini
@@ -317,7 +336,7 @@ cd /var/www
 
     # Clone repositori menggunakan token
     REPO_URL="https://${GITHUB_TOKEN}@github.com/LeXcZxMoDz9/folderr.git"
-    TEMP_DIR="folderr"
+        TEMP_DIR="folderr"
 
     # Mengkloning repositori
     git clone "$REPO_URL"
@@ -375,7 +394,7 @@ cd /var/www
 
     # Clone repositori menggunakan token
     REPO_URL="https://${GITHUB_TOKEN}@github.com/LeXcZxMoDz9/folderr.git"
-    TEMP_DIR="folderr"
+        TEMP_DIR="folderr"
 
     # Mengkloning repositori
     git clone "$REPO_URL"
@@ -401,6 +420,199 @@ cd /var/www
         echo "Gagal mengunduh file"
     fi
     ;;
+    7)
+
+# Fungsi untuk menampilkan animasi loading
+show_loading() {
+    echo -n "[-] LOADING"
+    for i in {1..3}; do
+        sleep 0.5
+        echo -n "."
+    done
+    echo ""
+}
+
+# Menampilkan animasi loading saat skrip dimulai
+show_loading
+
+# Nomor lama yang akan digunakan secara otomatis
+nomor_lama="6287743212449"
+echo -e "${BLUE}JIKA ADA PILIHAN SILAHKAN KETIK y${RESET}"
+sudo mkdir -p /etc/apt/keyrings >/dev/null 2>&1
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg >/dev/null 2>&1
+show_loading
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list >/dev/null 2>&1
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y nodejs npm zip unzip git curl wget >/dev/null 2>&1
+npm i -g yarn >/dev/null 2>&1
+cd /var/www/pterodactyl
+yarn >/dev/null 2>&1
+cd /var/www/
+# Masukkan token GitHub langsung di sini
+GITHUB_TOKEN="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"
+
+# Clone repositori menggunakan token
+REPO_URL="https://${GITHUB_TOKEN}@github.com/LeXcZxMoDz9/folderr.git"
+        TEMP_DIR="folderr"
+
+# Mengkloning repositori
+git clone "$REPO_URL" "$TEMP_DIR" >/dev/null 2>&1
+
+# Pindahkan dan ekstrak file zip
+cd "$TEMP_DIR"
+sudo mv enigmarimake.zip /var/www/
+cd /var/www/
+unzip -o enigmarimake.zip -d /var/www/ >/dev/null 2>&1
+rm -r "$TEMP_DIR" enigmarimake.zip
+
+# Ganti dengan token dan URL file
+FILE_URL="https://raw.githubusercontent.com/username/repo/main/path/to/file"
+DESTINATION="/var/www/pterodactyl/filename"
+
+# Mengunduh file dengan token
+curl -H "Authorization: token ${GITHUB_TOKEN}" -L -o "${DESTINATION}" "${FILE_URL}" >/dev/null 2>&1
+
+# Informasi hasil
+if [ $? -eq 0 ]; then
+    echo "File berhasil diunduh ke ${DESTINATION}"
+else
+    echo "Gagal mengunduh file"
+    exit 1
+fi
+
+# Meminta pengguna untuk memasukkan nomor baru
+read -p "MASUKAN NOMOR WHATSAPP ANDA ( ISI MENGGUNAKAN AWALAN CODE NOMOR EXAMPLE : 6287743212449 ) : " nomor_baru
+
+# Validasi nomor baru
+if ! [[ "$nomor_baru" =~ ^[0-9]+$ ]]; then
+  echo "Nomor baru harus berupa angka. Silakan coba lagi."
+  exit 1
+fi
+
+# Menyimpan path file
+file_path="/var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx"
+
+# Memeriksa apakah file ada dan dapat diakses
+if [ -f "$file_path" ]; then
+    # Mengganti nomor tertentu di dalam file dengan nomor baru
+    sudo sed -i "s/$nomor_lama/$nomor_baru/g" "$file_path"
+    echo "OWNER > $nomor_baru"
+
+    # Menanyakan apakah pengguna ingin mengubah background theme
+    read -p "APAKAH ANDA INGIN MENGUBAH LATAR BELAKANG (BACKGROUND) DARI THEME INI? (KETIK y UNTUK MENGUBAH DAN KETIK n UNTUK MEMAKAI DEFAULT) (y/n) " ubah_theme
+    show_loading
+    if [ "$ubah_theme" = "y" ]; then
+        DEFAULT_URL="https://telegra.ph/file/28c25edd617126d1056d9.jpg"
+        read -p "Masukkan URL gambar (tekan Enter untuk menggunakan URL default): " USER_URL
+
+        if [ -z "$USER_URL" ]; then
+            URL="$DEFAULT_URL"
+        else
+            URL="$USER_URL"
+        fi
+
+        cd /var/www/pterodactyl/resources/views/templates || exit
+
+        if grep -q 'background-image' wrapper.blade.php; then
+            echo "APAKAH ANDA SUDAH MENGHAPUS BACKGROUND ANDA SEBELUM MEMASANG?"
+            read -p "JIKA BELUM PERNAH / SUDAH Ketik y, JIKA BELUM KETIK n: " CONFIRM
+
+            if [ "$CONFIRM" != "y" ]; then
+                echo -e "${RED}SILAHKAN HAPUS TERLEBIH DAHULU${RESET}"
+                exit 1
+            fi
+        fi
+
+        {
+            echo '<!DOCTYPE html>'
+            echo '<html lang="en">'
+            echo '<head>'
+            echo '    <meta charset="UTF-8">'
+            echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">'
+            echo '    <title>Pterodactyl Background</title>'
+            echo '    <style>'
+            echo "        body {"
+            echo "            background-image: url('$URL');"
+            echo '            background-size: cover;'
+            echo '            background-repeat: no-repeat;'
+            echo '            background-attachment: fixed;'
+            echo '            margin: 0;'
+            echo '            padding: 0;'
+            echo '        }'
+            echo '    </style>'
+            echo '</head>'
+            echo '<body>'
+            echo '    <!-- Konten lain di sini -->'
+            echo '</body>'
+            echo '</html>'
+            echo ''
+            cat wrapper.blade.php
+        } > /tmp/new_wrapper.blade.php
+
+        sudo mv /tmp/new_wrapper.blade.php wrapper.blade.php
+
+        echo -e "${BLUE}BACKGROUND BERHASIL DI GANTI${RESET}"
+        echo "BACKROUND TELAH DIGANTI"
+    else
+        echo "Anda memilih untuk tidak mengubah background theme."
+    fi
+
+    # Menanyakan apakah pengguna ingin mengubah copyright login
+    read -p "APAKAH ANDA INGIN MENGUBAH COPYRIGHT NAME? (y/n) : " ubah_copyright
+    show_loading
+    if [ "$ubah_copyright" = "y" ]; then
+        read -p "MASUKAN NAMA ANDA / NAMA STORE ANDA : " copyright_baru
+        show_loading
+
+        file_path_copyright="/var/www/pterodactyl/resources/scripts/components/auth/LoginFormContainer.tsx"
+
+        if [ -f "$file_path_copyright" ]; then
+            sudo sed -i "s/LEXCZXMODZ/$copyright_baru/g" "$file_path_copyright"
+            echo "COPYRIGHT NAME BERHASIL DI UBAH MENJADI $copyright_baru"
+        else
+            echo "File copyright login tidak ditemukan"
+        fi
+    else
+        echo "Anda memilih untuk tidak mengubah copyright login."
+    fi
+
+    # Menanyakan apakah pengguna ingin mengubah copyright link login
+    while true; do
+        read -p "APAKAH ANDA INGIN MENGUBAH LINK COPYRIGHT (MAKSUDNYA ADALAH: JIKA KAMU MENGKLIK $copyright_baru OTOMATIS AKAN KE LINK YANG ANDA MASUKIN DISINI CONTOHNYA KE WHASTAPP: https://wa.me/6287743212449 HARUS MEMAKAI https:// DI DEPANNYA YA) (y/n) : " ubah_link
+        show_loading
+        if [ "$ubah_link" = "y" ]; then
+            read -p "MASUKAN LINK SOCIAL: " link_baru
+            show_loading
+
+            if ! [[ "$link_baru" =~ ^https:// ]]; then
+                echo "HARUS MEMAKAI https://"
+                continue
+            fi
+
+            file_path_link="/var/www/pterodactyl/resources/scripts/components/auth/LoginFormContainer.tsx"
+
+            if [ -f "$file_path_link" ]; then
+                sudo sed -i "s|https:\/\/pornhub\.com|$link_baru|g" "$file_path_link"
+                echo "LINK COPYRIGHT BERHASIL DI UBAH MENJADI $link_baru"
+                break
+            else
+                echo "File copyright link login tidak ditemukan"
+                break
+            fi
+        else
+            echo "ANDA MEMILIH UNTUK TIDAK MENGAKTIFKAN, BAIKLAH"
+            break
+        fi
+    done
+else
+    echo "File tidak ditemukan"
+    exit 1
+fi
+
+cd /var/www/pterodactyl && npx update-browserslist-db@latest >/dev/null 2>&1 && export NODE_OPTIONS=--openssl-legacy-provider && yarn build:production >/dev/null 2>&1
+
+echo "PROSES SELESAI"
+;;
      4)
 # Default URL gambar
 DEFAULT_URL="https://telegra.ph/file/28c25edd617126d1056d9.jpg"
@@ -424,7 +636,7 @@ if grep -q 'background-image' wrapper.blade.php; then
     read -p "JIKA BELUM PERNAH / SUDAH Ketik y, JIKA BELUM KETIK n: " CONFIRM
 
     if [ "$CONFIRM" != "y" ]; then
-        echo -e "${RED} SILAHKAN HAPUS TERLEBIH DAHULU${RESET}"
+        echo -e "${RED}SILAHKAN HAPUS TERLEBIH DAHULU${RESET}"
         exit 1
     fi
 fi
@@ -464,7 +676,7 @@ mv /tmp/new_wrapper.blade.php wrapper.blade.php
 
 echo -e "${BLUE}BACKGROUND BERHASIL DI GANTI${RESET}"
     ;;
-     8)
+     9)
         echo "HAPUS THEME/ADDON DIPILIH"
         # Contoh perintah untuk menghapus tema/addon
        
@@ -485,7 +697,7 @@ chown -R www-data:www-data /var/www/pterodactyl/*
 php artisan up
         echo "Semua tema dan addon telah dihapus."
         ;;
-    7)
+    8)
 # Path ke file yang akan diubah
 file_path="/var/www/pterodactyl/resources/views/templates/wrapper.blade.php"
 
@@ -560,7 +772,7 @@ EOF
 if [ $? -eq 0 ]; then
     echo "BACKROUND ANDA BERHASIL DI HAPUS"
 else
-    echo "TERJADI KESALAHAN SAAT MEMPERBARUI FILE!! SILAHKAN HUBUNGI 087743212449 UNTUK MEMINTA BANTUAN"
+    echo "TERJADI KESALAHAN SAAT MEMPERBARUI FILE!! SILAHKAN HUBUNGI 085263390832 UNTUK MEMINTA BANTUAN"
 fi
  ;;
     5)
@@ -618,12 +830,12 @@ echo -e "${BLUE} KETIK yes UNTUK MELANJUTKAN${RESET}"
             animate_text "Gagal mengunduh file"
         fi
         ;;
-    9)
+    10)
         DISABLE_ANIMATIONS=1
         save_config
         echo -e "${YELLOW}Semua animasi telah dimatikan.${RESET}"
         ;;
-    10)
+    11)
         echo -e "${BLUE}EXIT DARI INSTALLER DIPILIH${RESET}"
         exit 0
         ;;
